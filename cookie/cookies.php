d7df8dd2f879ae5a027bcc2660eba65fac6d881e
zuza347 <zuzanna.laton347@gmail.com>
2023-10-06 11:43:58 +0200

    cookie

--- cookie/cookies.php
+++ cookie/cookies.php
@@ -1,9 +1,10 @@
 <?php
 $cookie_name="user";
+setcookie($cookie_name,"",time()-3600);
 if(!isset($_COOKIE[$cookie_name])){
-    echo "cookie named'".$cookie_name. "'is not set!";
+    echo"cookie named <br>'".$cookie_name."'is not set!";
 }else{
-    echo"cookie'". $cookie_name. "'is set<br>";
-    echo "value is: ". $_COOKIE[$cookie_name];
+    echo "cookie'".$cookie_name."'is set!<br>";
+    echo "Value is: ".$_COOKIE[$cookie_name];
 }
 ?> 
