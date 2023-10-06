c94bdf5ab62f0a5906d4e21e5f0f9f541f042cf3
zuza347 <zuzanna.laton347@gmail.com>
2023-10-06 13:19:48 +0200

    wyrazenia

--- wyrazenia_regularne/wyrazenia7.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $wzorzec='/([0123]?/d)+[-\.\/]+([01]?\d)+[-\.\/]+(\d{4})';
-    $tekst= "12.04.2023 data";
-    if(preg_match($wzorzec,$tekst,$matches))
-        echo "znaleziono kod".$matches[0];
-    else 
-        echo "nie znaleziono kodu <br>";
-echo "znaleziono:".preg_match_all($wzorzec,$tekst);
-?>
