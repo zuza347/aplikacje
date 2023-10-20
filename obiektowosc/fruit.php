96eae1b65c3b655e0df94ff2c5e512d7be97e438
zuza347 <zuzanna.laton347@gmail.com>
2023-10-20 07:49:56 +0200

    obiektowosc

---
+++ obiektowosc/fruit.php
@@ -0,0 +1,12 @@
+<?php
+    class fruit{
+        public $name;
+        public $color;
+        function set_name($name){
+            $this->name=$name;
+        }
+        function get_name(){
+            return $this->name;
+        }
+    }
+?>
